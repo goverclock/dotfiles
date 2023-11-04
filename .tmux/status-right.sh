b37e3fdf90f6c72f0e76ecca874229fb576a131e
goverclock <goverclock@outlook.com>
2023-11-04 11:09:53 +0800

    tmux status bar script based on OS

--- .tmux/status-right.sh
+++ .tmux/status-right.sh
@@ -8,8 +8,17 @@ function upt() {
 }
 
 function main() {
-	s1=`temperature`
+	s1='lol'
 	s2=`upt`
+
+	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		s1=`temperature`
+	elif [[ "$OSTYPE" == "darwin"* ]]; then
+		s1=''
+	else
+		echo "~/.tmux/status-right.sh: unknown os"
+	fi
+
 	echo $s1 $s2
 }
 
