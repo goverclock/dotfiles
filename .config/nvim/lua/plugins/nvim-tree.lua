29c2ec13ed8516e467270b798cd74876d4fc7997
goverclock <goverclock@outlook.com>
2024-09-17 16:24:23 +0800

    nvim do not enter parent directory with mouse

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -15,27 +15,27 @@ local function my_on_attach(bufnr)
 	-- see https://github.com/nvim-tree/nvim-tree.lua/blob/94c7c810af205c0f00c8f105dcf490c8eb17658a/doc/nvim-tree-lua.txt#L2033
 	-- vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
 	-- vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
-	-- vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
-	local vkm = vim.keymap
-	vkm.set('n', 'h', api.node.navigate.parent, opts('Parent Directory'))
-	vkm.set('n', 'l', function()
+
+	local open_not_parent = function()
 		local node = api.tree.get_node_under_cursor()
-		--local t = api.tree.get_nodes()
-		--print(vim.notify(vim.inspect(node)))
-		if node.name == ".." then	-- prevent entering parent directory accidently
+		if node.name == ".." then -- prevent entering parent directory accidently
 			return
 		end
 		api.node.open.edit()
 		if node.type ~= "directory" then
-			vim.cmd('wincmd p')		-- back to nvim-tree focus, rather than the opened file
+			vim.cmd('wincmd p') -- back to nvim-tree focus, rather than the opened file
 		end
-	end, opts('Open and Stay in Tree'))
+	end
+	local vkm = vim.keymap
+	vkm.set('n', 'h', api.node.navigate.parent, opts('Parent Directory'))
+	vkm.set('n', 'l', open_not_parent, opts('Open and Stay in Tree'))
 
-	vkm.del('n', '<C-e>', { buffer = bufnr })	-- prevent nvim-tree from opening in place, so that to move view up and down
+	-- prevent nvim-tree from opening in place, so that to move view up and down
+	vkm.del('n', '<C-e>', { buffer = bufnr })
 
 	-- use single click rather then double click to open file
 	vkm.del('n', '<2-LeftMouse>', { buffer = bufnr })
-	vkm.set('n', '<LeftRelease>', api.node.open.edit, opts('Open'))
+	vkm.set('n', '<LeftRelease>', open_not_parent, opts('Open'))
 end
 
 require("nvim-tree").setup {
@@ -54,13 +54,3 @@ require("nvim-tree").setup {
 		},
 	},
 }
-
--- vim.api.nvim_create_autocmd("BufEnter", {
--- 	nested = true,
--- 	callback = function()
--- 		if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
--- 			vim.cmd "quit"
--- 		end
--- 	end
--- })
-
