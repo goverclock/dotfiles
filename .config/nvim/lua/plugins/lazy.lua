9296d991a1bcf64fd3467d8ca0d49cc28fd7ccf6
goverclock <goverclock@outlook.com>
2026-02-08 19:08:41 +0800

    update nvim-telescope to fix issue https://github.com/nvim-telescope/telescope.nvim/issues/3360
    Signed-off-by: goverclock <goverclock@outlook.com>

--- .config/nvim/lua/plugins/lazy.lua
+++ .config/nvim/lua/plugins/lazy.lua
@@ -32,7 +32,7 @@ require("lazy").setup({
 		config = function()
 			require("plugins/telescope")
 		end,
-		tag = '0.1.2',
+		tag = '0.1.8',
 		-- or                              , branch = '0.1.x',
 		dependencies = { 'nvim-lua/plenary.nvim' },
 	},
